a1474134781821cb3fb9054d8ff64cbe12851ec3
unknown <wafiwafiwafi90@gmail.com>
2023-11-06 03:02:16 -0800

    expree level 2 cara post put delete lewat postman

--- index.js
+++ index.js
@@ -2,14 +2,30 @@
 const express = require('express')
 const app = express()
 const port = 1104
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 app.get('/',(req,res) => {
     res.send('hello world')
 })
+app.get('/hello',(req,res) => {
+    console.log({ infoParam : req.query.umur});
+    res.send('hello gaesss')
+})
+// req = dari luar ke dalam (dri postman ke server)
+// res = dari dalam ke luar (apa2 dri sistem)
 app.post ('/login',(req,res) => {
-    if (req.name === "wafi") {
-        res.send("approved")
-    }
+    console.log({ requestFromOutside : req.body });
+    res.send ("Login Berhasil")
+})
+app.put ('/login',(req,res) => {
+    console.log({ requestFromOutside : req.body });
+    res.send ("username telah berubah")
+})
+app.delete ('/login',(req,res) => {
+    console.log({ requestFromOutside : req.body });
+    res.send ("username tidak ada")
 })
 
 app.listen(port, () => {
