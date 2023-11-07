809d99e31d835397b0b0dd9ecebe78b7a1b9bebe
unknown <wafiwafiwafi90@gmail.com>
2023-11-07 04:55:38 -0800

    database show in browser

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
-const { Connectiontime } = require('./connection')
 const express = require('express')
 const app = express()
 const port = 1104
 const bodyParser = require('body-parser')
+const db = require('./connection')
+const response = require('./response')
 
-
+// routes / URL /endpoint utama kita method Get
 app.use(bodyParser.json())
 
 app.get('/',(req,res) => {
-    res.send(Connectiontime)
+    const dataPegawai = "SELECT * FROM PEGAWAI"
+    db.query(dataPegawai,(err,result) => {
+        // hasil dari data mysql
+        response(200,result,"datanya disini",res)
+    });
 })
-app.get('/hello',(req,res) => {
-    console.log({ infoParam : req.query.umur});
-    res.send('hello gaesss')
-})
-// req = dari luar ke dalam (dri postman ke server)
-// res = dari dalam ke luar (apa2 dri sistem)
-app.post ('/login',(req,res) => {
-    console.log({ requestFromOutside : req.body });
-    res.send ("Login Berhasil")
-})
-app.put ('/login',(req,res) => {
-    console.log({ requestFromOutside : req.body });
-    res.send ("username telah berubah")
-})
-app.delete ('/login',(req,res) => {
-    console.log({ requestFromOutside : req.body });
-    res.send ("username tidak ada")
+app.get('/findpegawai',(req,res) => {
+    const pegawai = `SELECT nama FROM pegawai WHERE umur = ${req.query.umur} `
+    db.query(pegawai,(err,result) => {
+        response(200,result,"ini orangnya",res)
+    })
+    console.log('find nip : ', req.query.umur)
+
 })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 })
-
